b011b6d862f37f5eb69fd5216b7ec5bc8c7f874b
Abhishek Agarwal <abhishek.agarwal@npl.co.uk>
2021-07-22 15:20:25 +0100

    Add simple circuit example

---
+++ examples/simple_circuit.py
@@ -0,0 +1,44 @@
+import sys
+
+for entry in sys.path:
+    print(entry)
+
+from lib import HardwareAbstractionLayer, ProjectqQuantumSimulator
+from lib.hal import command_creator, measurement_unpacker
+
+# set up HAL using projectq backend for 3 qubits
+hal = HardwareAbstractionLayer(
+    ProjectqQuantumSimulator(3)
+)
+
+# prepare commands
+circuit = [
+    ["STATE_PREPARATION", 0, 0],
+    ['X', 0, 0],
+    ['H', 0, 2],
+    ["T", 0, 0],
+    ["SX", 0, 1],
+    ["T", 0, 2],
+    ["S", 0, 2],
+    ["SWAP", 1, 2],
+    ["T", 0, 2],
+    ["INVS", 0, 2],
+    ['RZ', 672, 1],
+    ['SQRT_X', 0, 0],
+    ['PSWAP', 200, 0, 0, 1],
+    ["CNOT", 0, 2],
+    ["H", 0, 2],
+    ["PIXY", 458, 1],
+]
+
+# send commands to HAL
+for commands in circuit:
+
+    hal_cmd = command_creator(*commands)
+    hal.accept_command(hal_cmd)
+
+# send measure command to HAL and get encoded HAL result
+hal_result = hal.accept_command(command_creator(*['QUBIT_MEASURE', 0, 2]))
+
+# decode hal result and print qubit index, status, readout
+print(measurement_unpacker(hal_result))
