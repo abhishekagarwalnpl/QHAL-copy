cba06f01ca9850743bf6f4108e4ea924c64e5bd1
dan <dan.underwood88@gmail.com>
2021-09-15 13:38:25 +0000

    added function and test

--- test/test_hal_messages.py
+++ test/test_hal_messages.py
@@ -2,6 +2,8 @@ import unittest
 
 from qhal.hal._commands import (command_creator,
                                command_unpacker,
+                               measurement_creator,
+                               measurement_unpacker,
                                _OPCODES)
 
 
@@ -42,6 +44,17 @@ class HALTest(unittest.TestCase):
                                         [qubit0, qubit1]
                                     )
                                 )
+
+    def test_measurement_creator_unpacker(self):
+
+        for idx in range(8):
+            for status in range(8):
+                for res in range(2):
+                    args = (idx, status, res)
+                    self.assertEqual(
+                        args, 
+                        measurement_unpacker(measurement_creator(*args))
+                    )
 
 
 if __name__ == "__main__":
