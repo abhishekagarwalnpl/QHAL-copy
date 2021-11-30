34b2cdebd9619b8f4dddee52d690c1850c38e55b
Naomi Solomons <naomi.solomons@riverlane.com>
2021-11-30 10:57:21 +0000

    Added roundtrip utils test

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,15 +1,16 @@
 import numpy as np
 import unittest
 
-from qhal.hal._utils import angle_binary_representation
+from qhal.hal._utils import (angle_binary_representation,
+                             binary_angle_conversion)
 
 
 class UtilsTest(unittest.TestCase):
-    """basic tests for HAL util functions.
+    """Basic tests for HAL util functions.
     """
 
     def test_angle_binary_conversion(self):
-        """Thest the conversion of angles to 16-bit representation."""
+        """Test the conversion of angles to 16-bit representation."""
 
         test_cases = {
             0: angle_binary_representation(0),
@@ -36,6 +37,29 @@ class UtilsTest(unittest.TestCase):
 
         for expected, calculated in test_cases.items():
             self.assertEqual(expected, calculated)
+
+
+    def test_angle_binary_roundtrip(self):
+        """Test that conversions between angle and binary and 
+           back again give the same results.
+        """
+
+        binary_tests = [0, 8192, 10923, 16384, 21845, 24576, 
+                        32768, 40960, 43691, 49152, 54613, 57344]
+        angle_tests = [0, np.pi/4, np.pi/3, np.pi/2, 2*np.pi/3,
+                       3*np.pi/4, np.pi, 5*np.pi/4, 4*np.pi/3,
+                       3*np.pi/2, 5*np.pi/3, 7*np.pi/4]
+
+        for bin_rep in binary_tests:
+            self.assertEqual(angle_binary_representation(
+                             binary_angle_conversion(bin_rep)),
+                             bin_rep)
+
+        for angle in angle_tests:
+            self.assertAlmostEqual(binary_angle_conversion(
+                             angle_binary_representation(angle)),
+                             angle, places=4)
+            #Binary to angle is not exact
 
 
 if __name__ == "__main__":
