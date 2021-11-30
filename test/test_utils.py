c6d2a109de583ae5a64fc8252e26fbe89738e262
Naomi Solomons <naomi.solomons@riverlane.com>
2021-11-30 11:23:45 +0000

    Added utils test

--- test/test_utils.py
+++ test/test_utils.py
@@ -38,6 +38,41 @@ class UtilsTest(unittest.TestCase):
         for expected, calculated in test_cases.items():
             self.assertEqual(expected, calculated)
 
+    def test_binary_to_angle_conversion(self):
+        """Test the conversion of 16-bit binary to angles.
+           Note that the conversion is only accurate up to 4 d.p."""
+
+        # We also test some values outside of the usual 16-bit 
+        # uint that the HAL would expect, as the conversion
+        # is still accurate and these are sometimes useful.
+
+        test_cases = {
+            0: binary_angle_conversion(0),
+            np.pi/4: binary_angle_conversion(8192),
+            np.pi/3: binary_angle_conversion(10923),
+            np.pi/2: binary_angle_conversion(16384),
+            2 * np.pi/3: binary_angle_conversion(21845),
+            3 * np.pi/4: binary_angle_conversion(24576),
+            np.pi      : binary_angle_conversion(32768),
+            5 * np.pi/4: binary_angle_conversion(40960),
+            4 * np.pi/3: binary_angle_conversion(43691),
+            3 * np.pi/2: binary_angle_conversion(49152),
+            5 * np.pi/3: binary_angle_conversion(54613),
+            7 * np.pi/4: binary_angle_conversion(57344),
+            2 * np.pi: binary_angle_conversion(65536),
+            2 * np.pi + np.pi/4: binary_angle_conversion(73728),
+            2 * np.pi + np.pi: binary_angle_conversion(98304),
+            2 * np.pi + 7 * np.pi/4: binary_angle_conversion(122880),
+            -2 * np.pi: binary_angle_conversion(-65536),
+            -7 * np.pi/4: binary_angle_conversion(-57344),
+            -np.pi: binary_angle_conversion(-32768),
+            -np.pi/4: binary_angle_conversion(-8192),
+        }
+
+        for expected, calculated in test_cases.items():
+            self.assertAlmostEqual(expected, calculated, places=4)
+
+
 
     def test_angle_binary_roundtrip(self):
         """Test that conversions between angle and binary and 
